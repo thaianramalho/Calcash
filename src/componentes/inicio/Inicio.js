374694611af297ee3087bd63dc5710e50226f867
Thaian Ramalho <thaianramalho32@gmail.com>
2022-10-02 00:07:44 -0300

    link fonts

--- src/componentes/inicio/Inicio.js
+++ src/componentes/inicio/Inicio.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Inicio(){
     return(
-        <div>
+        <div id='principal'>
           <div id='title'>
             <p id='blue'>
             Calc <img src='/img/Calculadora.png' id='miniIcone' alt='Calculadora'></img>
@@ -14,7 +14,9 @@ function Inicio(){
         <div id='textoComum'>
           <p>Possuímos ferramentas que irão facilitar seu dia a dia nos<br></br> cálculos de lucro por venda em sua Loja Virtual do<br></br> <span id='green'>Mercado Livre, Shopee, B2W, entre outros!</span></p>
           </div>
-          <img id='desenhosvg' src='/img/DesenhoSVG.png' alt='calcash'></img>
+          <div id='desenhosvg'>
+            <img src='/img/DesenhoSVG.png' alt='calcash'></img>
+          </div>
       </div>
     )
 }
