733cdd27752a0b9690f366a7b7a5d71127c1f8a7
Matheus Bomtempo <matheusbarbacena@gmail.com>
2022-10-02 18:51:24 -0300

    so falta posicionar

--- src/componentes/inicio/Inicio.js
+++ src/componentes/inicio/Inicio.js
@@ -1,21 +1,30 @@
 import './Inicio.css'
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faCalculator, faSackDollar,} from '@fortawesome/free-solid-svg-icons';
+
+
+
 function Inicio(){
     return(
         <div id='principal'>
           <div id='title'>
             <p id='blue'>
-            Calc <img src='/img/Calculadora.png' id='miniIcone' alt='Calculadora'></img>
+            Calc <FontAwesomeIcon id='calculator' icon={faCalculator} />
+            
             <br></br>
-            <span id='green'>Cash </span><img src='/img/Dinheiro.png' id='miniIcone'alt='Dinheiro'></img>
+            <span id='green'>Cash <FontAwesomeIcon id='sack' icon={faSackDollar}/></span>
             </p>
         </div>
         <div id='textoComum'>
           <p>Possuímos ferramentas que irão facilitar seu dia a dia nos<br></br> cálculos de lucro por venda em sua Loja Virtual do<br></br> <span id='green'>Mercado Livre, Shopee, B2W, entre outros!</span></p>
           </div>
           <div id='desenhosvg'>
-            <img src='/img/DesenhoSVG.png' alt='calcash'></img>
+          <svg width="1367" height="688" viewBox="0 0 1367 688" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 687.5L1021.5 519.494L1366.5 0V687.5H821.5H0Z" fill="#5850FE"/>
+</svg>
           </div>
       </div>
     )
