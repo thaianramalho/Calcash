039ab3e6905a630d49020abf1b6f58f8abeedbab
Thaian Ramalho <thaianramalho32@gmail.com>
2022-10-01 22:09:41 -0300

    adicionado sub texto
    adicionado texto que fica em baixo do letreiro grande que diz mais sobre as funcionalidades do site.

--- src/componentes/inicio/Inicio.js
+++ src/componentes/inicio/Inicio.js
@@ -3,9 +3,17 @@ import React from 'react';
 
 function Inicio(){
     return(
-        <div id='title'>
-         <h1 id='blue'>Calc</h1>
-         <h1 id='green'>Cash</h1>
+        <div>
+          <div id='title'>
+            <p id='blue'>
+            Calc <img src='/img/Calculadora.png' id='miniIcone' alt='Calculadora'></img>
+            <br></br>
+            <span id='green'>Cash </span><img src='/img/Dinheiro.png' id='miniIcone'alt='Dinheiro'></img>
+            </p>
+        </div>
+        <div id='textoComum'>
+          <p>Possuímos ferramentas que irão facilitar seu dia a dia nos<br></br> cálculos de lucro por venda em sua Loja Virtual do<br></br> <span id='green'>Mercado Livre, Shopee, B2W, entre outros!</span></p>
+          </div>
       </div>
     )
 }
