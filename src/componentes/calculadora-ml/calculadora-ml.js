c82ad0b2a5d1e7340a9fbafde3b96643fac8cc3d
Thaian Ramalho <thaianramalho32@gmail.com>
2022-10-07 01:06:09 -0300

    criado repositorio calc ml

---
+++ src/componentes/calculadora-ml/calculadora-ml.js
@@ -0,0 +1,10 @@
+import './calculadora-ml.css'
+import React from 'react';
+
+export default function(){
+    return(
+        <div>
+            <input type={number} name='quantidade' id='quantidade' placeholder='Quantidade'></input>
+        </div>
+    )
+}
