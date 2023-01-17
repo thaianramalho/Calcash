e916bf333551d6c640a3f959fef5d26da77614a7
Thaian Ramalho <80857637+thaianramalho@users.noreply.github.com>
2023-01-17 12:22:47 -0300

    alterado calculadora.js

--- src/componentes/Calculadora/Calculadora.js
+++ src/componentes/Calculadora/Calculadora.js
@@ -3,30 +3,192 @@ import "./Calculadora.css";
 import { useState } from "react";
 import Navbar2 from "../Navbar2/Navbar2";
 
-const Calculadora = () => {
-  const [custo, setCusto] = useState(0.0);
-  const [notaFiscal, setNotaFiscal] = useState(0.0);
-  const [despesas, setDespesas] = useState(0.0);
-  const [frete, setFrete] = useState(0.0);
-  const [tarifa, setTarifa] = useState(0.0);
-  const [margemLucro, setMargemLucro] = useState(0.0);
-  const [resultado, setResultado] = useState(0.0);
-  const [resultadoLucro, setResultadoLucro] = useState(0.0);
+const Custo = (custo, setCusto) => {
+  return (
+    <div className="inputs">
+      <p>Custo do produto:</p>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">
+            R$
+          </span>
+        </div>
+        <input
+          type="number"
+          id="custo"
+          value={custo}
+          min={0}
+          required
+          onChange={(e) => setCusto(e.target.value)}
+          className="form-control"
+          placeholder="Insira o valor"
+          aria-label="Insira o valor"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+    </div>
+  );
+};
 
-  const calcular = (
-    custo,
-    notaFiscal,
-    despesas,
-    frete,
-    tarifa,
-    margemLucro
-  ) => {
-    var custoTotal =
-      100 / (100 - (despesas + notaFiscal + tarifa + margemLucro));
-    var precoVenda = (custo + frete) * custoTotal;
-    var lucroPorVenda = (margemLucro / 100) * precoVenda;
-    return lucroPorVenda;
+const Imposto = (notaFiscal, setNotaFiscal) => {
+  return (
+    <div className="inputs">
+      <p>Imposto NF-E:</p>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">
+            %
+          </span>
+        </div>
+        <input
+          type="number"
+          id="imposto"
+          value={notaFiscal}
+          min={0}
+          required
+          onChange={(e) => setNotaFiscal(e.target.value)}
+          className="form-control"
+          placeholder="Insira o valor"
+          aria-label="Insira o valor"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Despesas = (despesas, setDespesas) => {
+  return (
+    <div className="inputs">
+      <p>Despesas de venda:</p>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">
+            %
+          </span>
+        </div>
+        <input
+          type="number"
+          id="despesas"
+          value={despesas}
+          min={0}
+          required
+          onChange={(e) => setDespesas(e.target.value)}
+          className="form-control"
+          placeholder="Insira o valor"
+          aria-label="Insira o valor"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Tarifa = (tarifa, setTarifa) => {
+  return (
+    <div className="inputs">
+      <p>Tarifa do anúncio:</p>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">
+            %
+          </span>
+        </div>
+        <input
+          type="number"
+          id="classico"
+          value={tarifa}
+          min={0}
+          required
+          onChange={(e) => setTarifa(e.target.value)}
+          className="form-control"
+          placeholder="Insira o valor"
+          aria-label="Insira o valor"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Frete = (frete, setFrete) => {
+  return (
+    <div className="inputs">
+      <p>Valor do frete:</p>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">
+            R$
+          </span>
+        </div>
+        <input
+          type="number"
+          id="frete"
+          value={frete}
+          min={0}
+          required
+          onChange={(e) => setFrete(e.target.value)}
+          className="form-control"
+          placeholder="Insira o valor"
+          aria-label="Insira o valor"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Lucro = (margemLucro, setMargemLucro) => {
+  return (
+    <div className="inputs">
+      <p>Margem de lucro:</p>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">
+            %
+          </span>
+        </div>
+        <input
+          type="number"
+          id="margemLucro"
+          value={margemLucro}
+          required
+          min={0}
+          onChange={(e) => setMargemLucro(e.target.value)}
+          className="form-control"
+          placeholder="Insira o valor"
+          aria-label="Insira o valor"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+    </div>
+  );
+};
+
+const calcular = (despesas, notaFiscal, tarifa, margemLucro, custo, frete) => {
+  let custoTotal = 100 / (100 - (despesas + notaFiscal + tarifa + margemLucro));
+  let precoVenda = (custo + frete) * custoTotal;
+  return console.log(precoVenda.toFixed(2));
+};
+
+const Calc2 = () => {
+  let custoTotal = 0;
+  let precoVenda = 0;
+  let lucroPorVenda = 0;
+
+  const calcularLucro = () => {
+    custoTotal = 100 / (100 - (despesas + notaFiscal + tarifa + margemLucro));
+    precoVenda = (custo + frete) * custoTotal;
+    lucroPorVenda = (margemLucro / 100) * precoVenda;
+    return lucroPorVenda.toFixed(2);
   };
+
+  const [custo, setCusto] = useState("");
+  const [notaFiscal, setNotaFiscal] = useState("");
+  const [despesas, setDespesas] = useState("");
+  const [frete, setFrete] = useState("");
+  const [tarifa, setTarifa] = useState("");
+  const [margemLucro, setMargemLucro] = useState("");
 
   return (
     <>
@@ -38,161 +200,30 @@ const Calculadora = () => {
             Calculadora <br /> Mercado Livre
           </h2>
         </div>
-
         <form className="boxbox">
           <div className="box">
-            <div className="inputs">
-              <p>Custo do produto:</p>
-
-              <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon1">
-                    R$
-                  </span>
-                </div>
-                <input
-                  type="number"
-                  id="custo"
-                  value={custo}
-                  onChange={(e) => setCusto(e.target.value)}
-                  className="form-control"
-                  placeholder="Insira o valor"
-                  aria-label="Insira o valor"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
-            <div className="inputs">
-              <p>Imposto NF-E:</p>
-
-              <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon1">
-                    %
-                  </span>
-                </div>
-                <input
-                  type="number"
-                  id="imposto"
-                  value={notaFiscal}
-                  onChange={(e) => setNotaFiscal(e.target.value)}
-                  className="form-control"
-                  placeholder="Insira o valor"
-                  aria-label="Insira o valor"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
-
-            <div className="inputs">
-              <p>Despesas de venda:</p>
-
-              <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon1">
-                    %
-                  </span>
-                </div>
-                <input
-                  type="number"
-                  id="despesas"
-                  value={despesas}
-                  onChange={(e) => setDespesas(e.target.value)}
-                  className="form-control"
-                  placeholder="Insira o valor"
-                  aria-label="Insira o valor"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
-
-            <div className="inputs">
-              <p>Tarifa do anúncio:</p>
-
-              <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon1">
-                    %
-                  </span>
-                </div>
-                <input
-                  type="number"
-                  id="classico"
-                  value={tarifa}
-                  onChange={(e) => setTarifa(e.target.value)}
-                  className="form-control"
-                  placeholder="Insira o valor"
-                  aria-label="Insira o valor"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
-
-            <div className="inputs">
-              <p>Valor do frete:</p>
-
-              <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon1">
-                    R$
-                  </span>
-                </div>
-                <input
-                  type="number"
-                  id="frete"
-                  value={frete}
-                  onChange={(e) => setFrete(e.target.value)}
-                  className="form-control"
-                  placeholder="Insira o valor"
-                  aria-label="Insira o valor"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
-
-            <div className="inputs">
-              <p>Margem de lucro:</p>
-
-              <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon1">
-                    %
-                  </span>
-                </div>
-                <input
-                  type="number"
-                  id="margemLucro"
-                  value={margemLucro}
-                  onChange={(e) => setMargemLucro(e.target.value)}
-                  className="form-control"
-                  placeholder="Insira o valor"
-                  aria-label="Insira o valor"
-                  aria-describedby="basic-addon1"
-                />
-              </div>
-            </div>
+            {Custo(custo, setCusto)}
+            {Imposto(notaFiscal, setNotaFiscal)}
+            {Despesas(despesas, setDespesas)}
+            {Tarifa(tarifa, setTarifa)}
+            {Frete(frete, setFrete)}
+            {Lucro(margemLucro, setMargemLucro)}
           </div>
         </form>
-
         <div className="resultados">
           <div className="res" id="1">
             <h3>Preço da venda</h3>
-            <h2 className="lucroLiquido">{resultado}</h2>
+            <h2 className="lucroLiquido"></h2>
           </div>
 
           <div className="res">
             <h3>Lucro por venda</h3>
             <h2 className="lucroLiquido" id="2">
-              {resultadoLucro}
             </h2>
           </div>
         </div>
         <div className="botoes">
-          <button
-            type="submit"
-            onClick={calcular}
-            className="btn btn-primary btn-lg"
-          >
+          <button type="submit" onSubmit={calcular(despesas, notaFiscal, tarifa, margemLucro, custo, frete)} className="btn btn-primary btn-lg">
             Calcular
           </button>
           <button type="reset" className="btn btn-secondary btn-lg">
@@ -204,4 +235,4 @@ const Calculadora = () => {
   );
 };
 
-export default Calculadora;
+export default Calc2;
