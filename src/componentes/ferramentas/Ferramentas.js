6244339b12af97f83fc8703777a7ee03a644511f
Thaian Ramalho <80857637+thaianramalho@users.noreply.github.com>
2023-01-17 11:55:09 -0300

    Update Ferramentas.js

--- src/componentes/ferramentas/Ferramentas.js
+++ src/componentes/ferramentas/Ferramentas.js
@@ -1,89 +1,86 @@
-import './Ferramentas.css'
-import React from 'react';
+import "./Ferramentas.css";
+import React from "react";
 // import { useNavigate } from 'react-router-dom'
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { } from '@fortawesome/free-solid-svg-icons';
+import {} from "@fortawesome/free-solid-svg-icons";
 
+function Ferramentas() {
+  // const navigate = useNavigate();
+  return (
+    <div className="ferramentas" id="ferramenta">
+      <div className="titulo">
+        <h2>
+          Ferram<span>entas</span>
+        </h2>
+      </div>
 
-function Ferramentas(){
-    // const navigate = useNavigate();
-    return(
-
-        <div className='ferramentas' id='ferramenta'>
-
-            <div className='titulo'>
-                <h2>
-                    Ferram<span>entas</span>
-                </h2>
-            </div>
-
-            <div className='containerBorda'>
-                <div className='container-box'>
-                    
-                        <div className='boxEtexto'>
-                            
-                                
-                                    <Link to="/calculadora">
-                                        <a className='link'  >
-                                            <div  className='box'>
-                                                <div className='bg'></div>
-                                                <div className='img'> <img src='/img/mercadoLivre.png'></img> </div>
-                                            </div>
-                                        </a>
-                                    </Link>
-                                
-                            
-                            <a className='link' href="">
-                                <div className='texto'>CALCULADORA<br/> MERCADO LIVRE</div>
-                            </a>
-                        </div>
-                    
-                    
-                        <div className='boxEtexto'>
-                            <Link  to="/calculadora2">
-                                <a className='link' href="">
-                                    <div className='box'>
-                                        <div className='bg'></div>
-                                        <div className='img'> <img src='/img/shoope.png'></img> </div>
-                                    </div>
-                                </a>
-                            </Link>
-                            <a className='link' href="">
-                                <div className='texto'>CALCULADORA <br/> SHOPEE</div>
-                            </a>
-                        </div>
-                    
-
-
-                    <div className='boxEtexto'>
-                        <a className='link' href="#">
-                            <div className='box'>
-                                <div className='bg'></div>
-                                <div className='img'> <img src='/img/b2w.png'></img> </div>
-                            </div>
-                        </a>
-                
-                        <a className='link'  href="">
-                            <div className='texto'>CALCULADORA <br/> B2W</div>
-                        </a>
-                    </div>
+      <div className="containerBorda">
+        <div className="container-box">
+          <div className="boxEtexto">
+            <Link to="/calculadora">
+              <a className="link">
+                <div className="box">
+                  <div className="bg"></div>
+                  <div className="img">
+                    {" "}
+                    <img src="/img/mercadoLivre.png"></img>{" "}
+                  </div>
                 </div>
-            </div>
+              </a>
+            </Link>
 
-            
-            
+            <a className="link" href="">
+              <div className="texto">
+                CALCULADORA
+                <br /> MERCADO LIVRE
+              </div>
+            </a>
+          </div>
 
+          <div className="boxEtexto">
+            <Link to="/calculadora2">
+              <a className="link" href="">
+                <div className="box">
+                  <div className="bg"></div>
+                  <div className="img">
+                    {" "}
+                    <img src="/img/shoope.png"></img>{" "}
+                  </div>
+                </div>
+              </a>
+            </Link>
+            <a className="link" href="">
+              <div className="texto">
+                CALCULADORA <br /> SHOPEE
+              </div>
+            </a>
+          </div>
+
+          <div className="boxEtexto">
+            <a className="link" href="#">
+              <div className="box">
+                <div className="bg"></div>
+                <div className="img">
+                  {" "}
+                  <img src="/img/b2w.png"></img>{" "}
+                </div>
+              </div>
+            </a>
+
+            <a className="link" href="">
+              <div className="texto">
+                CALCULADORA <br /> B2W
+              </div>
+            </a>
+          </div>
         </div>
-
-
-        
-
-
-        )
+      </div>
+    </div>
+  );
 }
 
 export default Ferramentas;
